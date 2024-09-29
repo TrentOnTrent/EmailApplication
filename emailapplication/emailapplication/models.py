ba340a959931e5ec5821471d11afce905ed27fed
TrentOnTrent <Trentondunstan@hotmail.com>
2024-09-29 21:01:07 +1000

    Fixed errors

--- emailapplication/emailapplication/models.py
+++ emailapplication/emailapplication/models.py
@@ -1,8 +1,8 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
 from django.db import models
 
 
-class User(AbstractUser):
+class User(User):
     pass
 
 
